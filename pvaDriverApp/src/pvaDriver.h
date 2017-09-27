358a7edf407e8538e116da3baf1af481d2ba7e85
Mark Rivers <rivers@cars.uchicago.edu>
2017-09-27 11:15:24 -0500

    Changed DRIVER_REVISION from 1 to 3

--- pvaDriverApp/src/pvaDriver.h
+++ pvaDriverApp/src/pvaDriver.h
@@ -7,7 +7,7 @@
 #define PVAPvConnectionStatusString "PV_CONNECTION"
 
 #define DRIVER_VERSION      1
-#define DRIVER_REVISION     1
+#define DRIVER_REVISION     3
 #define DRIVER_MODIFICATION 0
 
 class pvaDriver;
